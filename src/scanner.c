774b133570d55bf29173f09b86261e6de1d56b6f
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-04-04 15:55:51 +0200

    scanner.c: Scaffolded

--- src/scanner.c
+++ src/scanner.c
@@ -1,30 +1,84 @@
 #include "tree_sitter/parser.h"
 #include "tree_sitter/alloc.h"
-#include "tree_sitter/array.h"
 
 enum TokenType {
     NEWLINE,
-    TOKEN_EOF
+    BLOCK_CLOSE,
+    BLOCK_CONTINUE,
+    QUOTE_MARKER,
+    H1_MARKER,
+    H2_MARKER,
+    H3_MARKER,
+    H4_MARKER,
+    H5_MARKER,
+    H6_MARKER,
+    THEMATIC_BREAK,
+    LIST_MARKER,
+    LIST_MARKER_PARENTHESIS,
+    LIST_MARKER_DOT,
+    CODE_BLOCK_START,
+    CODE_BLOCK_END,
+    METADATA_START,
+    METADATA_END,
+    TABLE_START,
+    TABLE_NEWLINE
 };
 
-void *tree_sitter_norsu_external_scanner_create() {
-    Scanner
+typedef enum {
+    QUOTE,
+    CODE_BLOCK,
+    LIST_ITEM,
+    LIST_ITEM_INDENTED_1,
+    LIST_ITEM_INDENTED_2,
+    LIST_ITEM_INDENTED_3,
+    LIST_ITEM_INDENTED_4,
+    LIST_ITEM_INDENTED_5,
+    LIST_ITEM_INDENTED_6,
+    LIST_ITEM_INDENTED_7,
+    LIST_ITEM_INDENTED_8
+} Block;
+
+typedef struct {
+    struct {
+        size_t size;
+        size_t capacity;
+        Block *items;
+    } open_blocks;
+
+    uint8_t state;
+    uint8_t matched;
+    uint8_t indentation;
+    uint8_t column;
+    
+    bool simulate;
+} Scanner;
+
+bool scan(
+    void *payload,
+    TSLexer *lexer,
+    const bool *valid_symbols
+) {
+    // TODO
+    return true;
 }
 
-void tree_sitter_norsu_external_scanner_destroy(void *payload) {
-    return;
+unsigned serialize(void *payload, char *buffer) {
+    // TODO
+    return 5;
 }
 
-unsigned tree_sitter_norsu_external_scanner_serialize(void *payload, char *buffer) {
-    return TREE_SITTER_SERIALIZATION_BUFFER_SIZE;
-}
-
-void tree_sitter_norsu_external_scanner_deserialize(
+void deserialize(
     void *payload,
     const char *buffer,
     unsigned length
 ) {
-    return;
+    // TODO
+}
+
+void *tree_sitter_norsu_external_scanner_create(void) {
+    Scanner *s = ts_malloc(sizeof(Scanner));
+    s->open_blocks.items = ts_calloc(1, sizeof(Block));
+    return s;
 }
 
 bool tree_sitter_norsu_external_scanner_scan(
@@ -32,5 +86,27 @@ bool tree_sitter_norsu_external_scanner_scan(
     TSLexer *lexer,
     const bool *valid_symbols
 ) {
-    return true;
+    Scanner *s = payload;
+    s->simulate = false;
+    return scan(s, lexer, valid_symbols);
+}
+
+unsigned tree_sitter_norsu_external_scanner_serialize(void *payload, char *buffer) {
+    Scanner *s = payload;
+    return serialize(s, buffer);
+}
+
+void tree_sitter_norsu_external_scanner_deserialize(
+    void *payload,
+    const char *buffer,
+    unsigned length
+) {
+    Scanner *s = payload;
+    deserialize(s, buffer, length);
+}
+
+void tree_sitter_norsu_external_scanner_destroy(void *payload) {
+    Scanner *s = payload;
+    ts_free(s->open_blocks.items);
+    ts_free(s);
 }
